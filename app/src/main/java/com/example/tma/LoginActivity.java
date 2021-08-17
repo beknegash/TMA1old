99c33aba7c31b65bb28c9302ab52f8b124913a12
b3k <bereketnegash1@gmail.com>
2021-08-17 12:53:19 +0300

    login two

--- app/src/main/java/com/example/tma/LoginActivity.java
+++ app/src/main/java/com/example/tma/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.tma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.textfield.TextInputEditText;
@@ -16,52 +17,54 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+         Intent intent = getIntent();
+
          username = findViewById(R.id.username);
-         password =  findViewById(R.id.password_edit_text);
+         password =  findViewById(R.id.password);
 
     }
-    private boolean validateUsername() {
-        String val = username.getEditText().getText().toString().trim();
-        String chekspaces = "\\A\\w{1,20}\\z";
-
-        if(val.isEmpty()) {
-            username.setError("Field can not be empty");
-            return  false;
-        }else if(val.length()>20){
-            username.setError("Username is too large");
-            return false;
-        }
-
-        else  if(!val.matches(chekspaces)){
-            username.setError("No white spaces are allowed");
-            return false;
-        }
-        else {
-            username.setError(null);
-            username.setErrorEnabled(false);
-            return true;
-        }
-    }
-    private boolean validatePassword() {
-        String val = password.getEditText().getText().toString().trim();
-        String chekspassword = "(?=.*[a-z][A-Z])"+     //any letter
-                                "(?=\\S+$)"+            //no white space
-                                ".{4,}"+                //at least 4 chx
-                                "$";
-
-        if(val.isEmpty()) {
-            password.setError("Field can not be empty");
-            return  false;
-        }
-
-        else  if(!val.matches(chekspassword)){
-            password.setError("Password should contain 4 characters");
-            return false;
-        }
-        else {
-            password.setError(null);
-            password.setErrorEnabled(false);
-            return true;
-        }
-    }
+//    private boolean validateUsername() {
+//        String val = username.getEditText().getText().toString().trim();
+//        String chekspaces = "\\A\\w{1,20}\\z";
+//
+//        if(val.isEmpty()) {
+//            username.setError("Field can not be empty");
+//            return  false;
+//        }else if(val.length()>20){
+//            username.setError("Username is too large");
+//            return false;
+//        }
+//
+//        else  if(!val.matches(chekspaces)){
+//            username.setError("No white spaces are allowed");
+//            return false;
+//        }
+//        else {
+//            username.setError(null);
+//            username.setErrorEnabled(false);
+//            return true;
+//        }
+//    }
+//    private boolean validatePassword() {
+//        String val = password.getEditText().getText().toString().trim();
+//        String chekspassword = "(?=.*[a-z][A-Z])"+     //any letter
+//                                "(?=\\S+$)"+            //no white space
+//                                ".{4,}"+                //at least 4 chx
+//                                "$";
+//
+//        if(val.isEmpty()) {
+//            password.setError("Field can not be empty");
+//            return  false;
+//        }
+//
+//        else  if(!val.matches(chekspassword)){
+//            password.setError("Password should contain 4 characters");
+//            return false;
+//        }
+//        else {
+//            password.setError(null);
+//            password.setErrorEnabled(false);
+//            return true;
+//        }
+//    }
 }
--- app/src/main/java/com/example/tma/MainActivity.java
+++ app/src/main/java/com/example/tma/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Build;
@@ -12,10 +13,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,13 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        logo = findViewById(R.id.logo);
+        
+
     }
 
+    public void enter(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
